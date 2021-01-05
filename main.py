1af1423360b4c1f7d11ae7823f2207929894018f
kocierik <kocierik@gmail.com>
2021-01-06 00:27:36 +0100

    feat: loading script

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from time import sleep
+import progressbar
+
 
 def printMenu():
   rows, cols = os.popen('stty size', 'r').read().split()
@@ -14,7 +17,8 @@ def printMenu():
   password = input("Enter senter password: ") 
   recipient = input("Enter recipient mail: ") 
   message = input("Enter the text to sent: ")
-  return senter,recipient,password
+  numberEmail = input("Enter the number email to sent: ")
+  return senter,recipient,password,message,numberEmail
   
 def seleniumDefinition():
   options = Options()
@@ -22,47 +26,65 @@ def seleniumDefinition():
   return webdriver.Firefox(options=options)
   
 def gmailSignIn(driver, email, password, recipient, message, numberEmail):
-    driver.implicitly_wait(6)
-    driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent')
-    driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
+  with progressbar.ProgressBar(max_value=100) as bar:
+    for i in range(1):
+      
+      driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent')
+      driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
 
-    mailLogin = driver.find_element_by_xpath('//*[@id="identifierId"]')
-    mailLogin.send_keys(email)
+      bar.update(14)
 
-    sendMailLogin = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]')
-    sendMailLogin.click()
-    time.sleep(2)
+      mailLogin = driver.find_element_by_xpath('//*[@id="identifierId"]')
+      mailLogin.send_keys(email)
 
-    psw = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input')
-    psw.send_keys(password)
+      bar.update(21)
 
-    sendPsw = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]')
-    sendPsw.click()
-    time.sleep(2)
-
-    driver.get('https://www.gmail.com/')
-    for x in range(numberEmail):
+      sendMailLogin = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]')
+      sendMailLogin.click()
       time.sleep(3)
-      sendNewMail = driver.find_element_by_xpath('/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div[1]/div/div')
-      sendNewMail.click()
-      time.sleep(5)
-      mailG = driver.find_element_by_class_name("vO")
-      mailG.send_keys(recipient)
-      time.sleep(2)
-      mailText = driver.find_element_by_class_name("editable")
-      mailText.send_keys(message)
-      time.sleep(1)
-      btnSend = driver.find_element_by_class_name("aoO")
-      btnSend.click()
-      print("Email sent")
-      time.sleep(5)
-    driver.close()
+
+      bar.update(37)
+
+      psw = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input')
+      psw.send_keys(password)
+
+      bar.update(45)
+
+      sendPsw = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]')
+      sendPsw.click()
+
+      bar.update(53)
+      time.sleep(3)
+
+      driver.get('https://www.gmail.com/')
+      for x in range(int(numberEmail)):
+        time.sleep(5)
+        bar.update(65)
+        sendNewMail = driver.find_element_by_xpath('/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div[1]/div/div')
+        sendNewMail.click()
+        time.sleep(7)
+        bar.update(73)
+        mailG = driver.find_element_by_class_name("vO")
+        mailG.send_keys(recipient)
+        bar.update(89)
+        time.sleep(5)
+        mailText = driver.find_element_by_class_name("editable")
+        mailText.send_keys(message)
+        time.sleep(2)
+        bar.update(95)
+        btnSend = driver.find_element_by_class_name("aoO")
+        btnSend.click()
+        bar.update(100)
+        print("email {} sent".format(x+1))
+  driver.close()
 
 def main():
-  senter, recipient, password, message = printMenu()
-  print(senter, " ", recipient)
+  senter, recipient, password, message, numberEmail = printMenu()
+  os.system("clear")
+  print(' senter: {}\n recipient: {}\n message: {}\n number Email {}\n\n'.format(senter,recipient,message,numberEmail))
   driver = seleniumDefinition()
-  gmailSignIn(driver, senter, password, recipient,message, 3)
+  print('Running...')
+  gmailSignIn(driver, senter, password, recipient,message, numberEmail)
       
 
 if __name__ == "__main__":
